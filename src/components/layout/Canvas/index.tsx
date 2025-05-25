c9b5b22af6ebf393f335bc979db723906614ed02
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-05-25 13:13:18 +0300

    add delete btn to remove an element from canvas

--- src/components/layout/Canvas/index.tsx
+++ src/components/layout/Canvas/index.tsx
@@ -1,23 +1,33 @@
 import { Stage, Layer, Transformer } from "react-konva";
 import { useRef, useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../../hooks/useRedux";
-import { selectElement, updateElement } from "../../../features/canvas/canvasSlice";
+import { deleteSelectedElement, selectElement, updateElement } from "../../../features/canvas/canvasSlice";
 import { ElementRenderer } from "../../ui/ElementRenderer";
 
 export function Canvas() {
   const elements = useAppSelector((state) => state.canvas.elements);
   const { stageWidth, stageHeight } = useAppSelector((state) => state.canvas);
   const dispatch = useAppDispatch();
-
   const selectedNodeRef = useRef<any>(null);
   const transformerRef = useRef<any>(null);
 
-  useEffect(() => {
-    if (transformerRef.current && selectedNodeRef.current) {
-      transformerRef.current.nodes([selectedNodeRef.current]);
-      transformerRef.current.getLayer()?.batchDraw();
-    }
-  }, [elements]);
+    useEffect(() => {
+      if (transformerRef.current && selectedNodeRef.current) {
+        transformerRef.current.nodes([selectedNodeRef.current]);
+        transformerRef.current.getLayer()?.batchDraw();
+      }
+    }, [elements]);
+
+    useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Delete") {
+        dispatch(deleteSelectedElement());
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
 
   return (
     <Stage width={stageWidth} height={stageHeight} style={{ border: "1px solid #ccc" }}>
