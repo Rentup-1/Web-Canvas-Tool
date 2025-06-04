adc66f9d0ed06e5d6a1381a63ce258c0ae98d81b
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-06-04 16:41:25 +0300

    handle percentage for x,y,width and height

--- src/hooks/usePercentConverter.ts
+++ src/hooks/usePercentConverter.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 
 export function usePercentConverter() {
   const toPercent = useCallback((value: number, total: number) => {
-    return Number(((value / total) * 100).toFixed(1)); // value of shape (width, height, x , y) -- total (width,height) of canvas elment
+    return Number(((value / total) * 100) / 100).toFixed(3); // value of shape (width, height, x , y) -- total (width,height) of canvas elment
   }, []);
 
   const fromPercent = useCallback((percent: number, total: number) => {
