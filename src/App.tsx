f886b33398f95f4a15852711e67b89384590c827
abdullah <aabdullah.zayed0099@gmail.com>
2025-08-07 15:03:24 +0300

    fead: wip apply tool colors

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,29 @@ import { Toaster } from "./components/ui/sonner";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
+import "@fontsource/roboto";
+import "@fontsource/open-sans";
+import "@fontsource/lato";
+import "@fontsource/montserrat";
+import "@fontsource/poppins";
+import "@fontsource/raleway";
+import "@fontsource/oswald";
+import "@fontsource/merriweather";
+import "@fontsource/playfair-display";
+import "@fontsource/nunito";
+import "@fontsource/ubuntu";
+import "@fontsource/pt-sans";
+import "@fontsource/inter";
+import "@fontsource/quicksand";
+import "@fontsource/source-sans-pro";
+import "@fontsource/cabin";
+import "@fontsource/rubik";
+import "@fontsource/fira-sans";
+import "@fontsource/inconsolata";
+import "@fontsource/manrope";
+
+import "./index.css";
+
 function App() {
   const stageRef = useRef<Konva.Stage | null>(null);
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import "./index.css";
 import { ThemeProvider } from "./components/ui/theme-provider";
-import "@fontsource/roboto";
-import "@fontsource/open-sans";
-import "@fontsource/lato";
-import "@fontsource/montserrat";
-import "@fontsource/poppins";
-import "@fontsource/raleway";
-import "@fontsource/oswald";
-import "@fontsource/merriweather";
-import "@fontsource/playfair-display";
-import "@fontsource/nunito";
-import "@fontsource/ubuntu";
-import "@fontsource/pt-sans";
-import "@fontsource/inter";
-import "@fontsource/quicksand";
-import "@fontsource/source-sans-pro";
-import "@fontsource/cabin";
-import "@fontsource/rubik";
-import "@fontsource/fira-sans";
-import "@fontsource/inconsolata";
-import "@fontsource/manrope";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
