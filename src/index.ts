b3a848bec92d14a94aef3185593c59f1e5436fea
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-08-12 20:39:49 +0300

    export more functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from "./App";
 import { buildTemplateFromJson } from "./components/Navbar/CanvasExportImport/MixerLogic";
+import { addColor, addFont } from "./features/branding/brandingSlice";
+import { setAspectRatio, setElements, setStageSize } from "./features/canvas/canvasSlice";
 
 // test
 export default App;
-export { buildTemplateFromJson };
+export { buildTemplateFromJson, setAspectRatio, setElements, setStageSize, addColor, addFont };
