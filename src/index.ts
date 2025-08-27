c6c7e5c04600904a65a8cfa6713a3e2b44fcae83
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-08-27 19:22:57 +0300

    fix vite config to run vercel production

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import App from "./App";
-import { buildTemplateFromJson } from "./components/Navbar/CanvasExportImport/MixerLogic";
-import { addColor, addFont } from "./features/branding/brandingSlice";
-import { setAspectRatio, setElements, setStageSize } from "./features/canvas/canvasSlice";
-
-// test2
-export default App;
-export { buildTemplateFromJson, setAspectRatio, setElements, setStageSize, addColor, addFont };
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,11 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
-
-
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"), 
-    },
-  },
-  build: {
-    lib: {
-      // The entry point of your library
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "TemplateDesigner", // A global variable name for the UMD build
-      fileName: (format) => `design-tool.${format}.js`,
-    },
-    rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
-      // into your library (they are peer dependencies)
-      external: ["react", "react-dom", "konva", "react-konva"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          konva: "Konva",
-          "react-konva": "ReactKonva",
-        },
-      },
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
