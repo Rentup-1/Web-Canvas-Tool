8a498eaab31dc06d3399b57aab36484997e042b7
Muhammed-1123 <maymab847@gmail.com>
2026-02-23 16:46:16 +0200

    style(navbar): update logo font to Satisfy

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,27 +10,33 @@ import { SaveToDataBase } from "./SaveToDataBase";
 import ToggleTheme from "./ToggleTheme";
 
 const Navbar: FC = () => {
-    const { theme } = useTheme();
-    return (
-        <div className="flex items-center justify-between px-4 py-4 overflow-hidden border-b">
-            <div className="flex items-center space-x-10">
-                <div className="flex  gap-2 items-center justify-center ">
-                    <img src={theme === "dark" ? LogoWhite : logoBlue} alt="Logo" className="w-15" />
-                    <p className="text-xl/0 font-medium">Canvas</p>
-                </div>
-                <HistoryControls />
-            </div>
-            <div>
-                <ElementGrouping />
-            </div>
-            <div className="flex items-center space-x-4">
-                <CanvasExportImport />
-                <SaveToDataBase />
-                <ShortcutsHelp />
-                <ToggleTheme />
-            </div>
+  const { theme } = useTheme();
+  return (
+    <div className="flex items-center justify-between px-4 py-4 overflow-hidden border-b">
+      <div className="flex items-center space-x-10">
+        <div className="flex  gap-2 items-center justify-center ">
+          <img
+            src={theme === "dark" ? LogoWhite : logoBlue}
+            alt="Logo"
+            className="w-15"
+          />
+          <p className="text-3xl font-bold font-['Satisfy'] bg-gradient-to-br from-primary to-blue-500 bg-clip-text text-transparent drop-shadow-sm">
+            Canvas
+          </p>
         </div>
-    );
+        <HistoryControls />
+      </div>
+      <div>
+        <ElementGrouping />
+      </div>
+      <div className="flex items-center space-x-4">
+        <CanvasExportImport />
+        <SaveToDataBase />
+        <ShortcutsHelp />
+        <ToggleTheme />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
